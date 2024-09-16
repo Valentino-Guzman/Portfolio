7e60c07bc893fa8ecd3f5ea4b89337bc827c7f7e
scino11 <prod.scino@gmail.com>
2024-09-16 17:46:36 -0300

    styles

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -3,11 +3,12 @@ import { GithubComponent } from "../icons/github/github.component";
 import { AngularComponent } from "../icons/angular/angular.component";
 import { NodeComponent } from "../icons/node/node.component";
 import { TailwindCssComponent } from "../icons/tailwind-css/tailwind-css.component";
+import { PreviewComponent } from "../icons/preview/preview.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [GithubComponent, AngularComponent, NodeComponent, TailwindCssComponent],
+  imports: [GithubComponent, AngularComponent, NodeComponent, TailwindCssComponent, PreviewComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
